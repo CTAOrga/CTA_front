a71d8dc2689bdb68e384b43b3fd2a6a3cbd9cacd
Charlie <charlie@camba.coop>
2025-10-16 08:45:28 -0300

    configuracion basica cypress

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+// cypress.config.js
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    // 👇 Aquí la configuración más importante
+    baseUrl: 'http://localhost:3000', // Reemplaza 3000 si tu app corre en otro puerto
+
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
