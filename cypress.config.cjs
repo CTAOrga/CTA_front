f88ef9a47d6d0b9ba9443421e38194b8e2241282
nicolastpi01 <nicolastpi10@gmail.com>
2025-10-20 19:56:59 -0300

    corriendo test y arreglado conf

--- cypress.config.cjs
+++ cypress.config.cjs
@@ -4,7 +4,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     // 👇 Aquí la configuración más importante
-    baseUrl: 'http://localhost:5173', // Reemplaza 3000 si tu app corre en otro puerto
+    baseUrl: "http://localhost:5173", // Reemplaza 3000 si tu app corre en otro puerto
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,55 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+import cypress from "eslint-plugin-cypress";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     extends: [
       js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
     },
   },
-])
+  {
+    files: ["cypress/**/*.cy.{js,jsx}", "cypress/**/?(*.)cy.{js,jsx}"],
+    plugins: { cypress },
+    rules: {
+      ...cypress.configs.recommended.rules,
+      "no-unused-expressions": "off", // útil para should('exist'), etc.
+      // 'cypress/unsafe-to-chain-command': 'off' // si te molesta el warning de .clear().type()
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.mocha, // describe/it/before/after
+        cy: "readonly",
+        Cypress: "readonly",
+        expect: "readonly",
+      },
+    },
+  },
+
+  // (opcional) Para archivos de config Node
+  {
+    files: ["cypress.config.*", "vite.config.*", "eslint.config.*"],
+    languageOptions: { globals: { ...globals.node } },
+  },
+]);
