b43ebee78fe2878bd233b788b5b4a3eead8a6c3a
nicolastpi01 <nicolastpi10@gmail.com>
2025-10-24 23:43:29 -0300

    agrego la pos de guardar videos (siempre), y capturas (solo cuando falla -el comp x defecto)

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  video: true, // habilita MP4 en "cypress run"
+  videosFolder: "cypress/videos", // opcional, default
+  screenshotsFolder: "cypress/screenshots", // opcional, default
   e2e: {
     baseUrl: "http://localhost:5173",
-    video: false,
     setupNodeEvents(on, config) {
       // plugins si necesitás
     },
